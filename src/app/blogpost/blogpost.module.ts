92cdc7b9ea1b273460f53467d6d92798a5b311a5
Nellia Salimova <salimova.nellia@gmail.com>
2019-10-24 16:30:05 +0300

    add blogpost-featured component

--- src/app/blogpost/blogpost.module.ts
+++ src/app/blogpost/blogpost.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BlogpostRoutingModule } from './blogpost-routing.module';
+import { BlogpostFeaturedComponent } from './blogpost-featured/blogpost-featured.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BlogpostFeaturedComponent],
   imports: [
     CommonModule,
     BlogpostRoutingModule
