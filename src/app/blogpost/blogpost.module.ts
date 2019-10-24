dd66da0ae3facc507ec7b299acf6d2d3f6f22e27
Nellia Salimova <salimova.nellia@gmail.com>
2019-10-24 10:15:49 +0300

    add blogpost module

---
+++ src/app/blogpost/blogpost.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BlogpostRoutingModule } from './blogpost-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BlogpostRoutingModule
+  ]
+})
+export class BlogpostModule { }
