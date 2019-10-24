80d81521ce43d7f0c91eb5d166ed83a5e13b28f1
Nellia Salimova <salimova.nellia@gmail.com>
2019-10-24 16:32:41 +0300

    add categories component

--- src/app/blogpost/blogpost.module.ts
+++ src/app/blogpost/blogpost.module.ts
@@ -6,10 +6,11 @@ import { BlogpostFeaturedComponent } from './blogpost-featured/blogpost-featured
 import { BlogpostListComponent } from './blogpost-list/blogpost-list.component';
 import { BlogpostDetailComponent } from './blogpost-detail/blogpost-detail.component';
 import { BlogpostRecentComponent } from './blogpost-recent/blogpost-recent.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 
 @NgModule({
-  declarations: [BlogpostFeaturedComponent, BlogpostListComponent, BlogpostDetailComponent, BlogpostRecentComponent],
+  declarations: [BlogpostFeaturedComponent, BlogpostListComponent, BlogpostDetailComponent, BlogpostRecentComponent, CategoriesComponent],
   imports: [
     CommonModule,
     BlogpostRoutingModule
