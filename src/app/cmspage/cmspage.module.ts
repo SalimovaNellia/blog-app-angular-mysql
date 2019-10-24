b170e1f21b2361f5ecaca21e6c5e0947836215b1
Nellia Salimova <salimova.nellia@gmail.com>
2019-10-24 16:34:05 +0300

    add cms page component

--- src/app/cmspage/cmspage.module.ts
+++ src/app/cmspage/cmspage.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CmspageRoutingModule } from './cmspage-routing.module';
+import { PageComponent } from './page/page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PageComponent],
   imports: [
     CommonModule,
     CmspageRoutingModule
