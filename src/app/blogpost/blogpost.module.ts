4a57da97a70824c0c06d0ff08bda7766ee20f336
Nellia Salimova <salimova.nellia@gmail.com>
2019-10-24 16:32:12 +0300

    add blogpost-recent component

--- src/app/blogpost/blogpost.module.ts
+++ src/app/blogpost/blogpost.module.ts
@@ -5,10 +5,11 @@ import { BlogpostRoutingModule } from './blogpost-routing.module';
 import { BlogpostFeaturedComponent } from './blogpost-featured/blogpost-featured.component';
 import { BlogpostListComponent } from './blogpost-list/blogpost-list.component';
 import { BlogpostDetailComponent } from './blogpost-detail/blogpost-detail.component';
+import { BlogpostRecentComponent } from './blogpost-recent/blogpost-recent.component';
 
 
 @NgModule({
-  declarations: [BlogpostFeaturedComponent, BlogpostListComponent, BlogpostDetailComponent],
+  declarations: [BlogpostFeaturedComponent, BlogpostListComponent, BlogpostDetailComponent, BlogpostRecentComponent],
   imports: [
     CommonModule,
     BlogpostRoutingModule
